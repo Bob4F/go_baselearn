c7e585cffabb32acdd17a0a592fc37a73f1f0799
13715166404@163.com <13715166404@163.com>
2019-09-30 10:42:59 +0800

    format for   code

--- learn/algorithm/RomaToint.go
+++ learn/algorithm/RomaToint.go
@@ -10,8 +10,7 @@
 	M             1000
  给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
  romaToInt1 成功
-
- */
+*/
 package main
 
 import (
@@ -20,35 +19,35 @@ import (
 
 func main() {
 	fmt.Println(romaToInt1("IV")) // 4
-	fmt.Println(romaToInt1("IX"))   // 9
+	fmt.Println(romaToInt1("IX")) // 9
 	fmt.Println()
 }
 
-var romaMapper map[string]int= map[string]int{
-	"I" : 1,
-	"V" : 5,
-	"X" : 10,
-	"L" : 50,
-	"C" : 100,
-	"D" : 500,
-	"M" : 1000,
+var romaMapper map[string]int = map[string]int{
+	"I": 1,
+	"V": 5,
+	"X": 10,
+	"L": 50,
+	"C": 100,
+	"D": 500,
+	"M": 1000,
 }
 
-func romaToInt1(roma string)int{
+func romaToInt1(roma string) int {
 	var count = 0
 	strLen := len(roma)
-	for i:=0;i < strLen; i++ {
+	for i := 0; i < strLen; i++ {
 		countNum := romaMapper[string(roma[i])]
-		fmt.Println(i,",countNum:",countNum)
+		fmt.Println(i, ",countNum:", countNum)
 		if i < strLen-1 {
 			dumpNext := romaMapper[string(roma[i+1])]
 			if dumpNext > countNum {
-				count=count+(dumpNext-countNum)
+				count = count + (dumpNext - countNum)
 				i++
 				continue
 			}
 		}
-		count=count+countNum
+		count = count + countNum
 	}
 	return count
 }
